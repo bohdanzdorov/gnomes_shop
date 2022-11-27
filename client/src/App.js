c5cf3272636c11cc486673e98f63eb8f464e4f29
BohdanZdorovets <zdorovetsbogdan@gmail.com>
2022-11-27 20:10:49 +0200

    divider with search textfield added

--- client/src/App.js
+++ client/src/App.js
@@ -14,9 +14,7 @@ import ListItem from '@mui/material/ListItem';
 import ListItemButton from '@mui/material/ListItemButton';
 import ListItemIcon from '@mui/material/ListItemIcon';
 import ListItemText from '@mui/material/ListItemText';
-import InboxIcon from '@mui/icons-material/MoveToInbox';
-import MailIcon from '@mui/icons-material/Mail';
-
+import TextField from '@mui/material/TextField';
 
 import MenuIcon from '@mui/icons-material/Menu';
 
@@ -47,130 +45,121 @@ function App() {
       onKeyDown={toggleDrawer(anchor, false)}
     >
 
-      <List onClick={toggleDrawer(anchor, false)}>
-        <ListItem sx = {{pt: 2, pl: 3, pb: 0}}>
+      <List>
+        <ListItem sx={{ pt: 2, pl: 3, pb: 0 }}>
           <ListItemText>AAA</ListItemText>
         </ListItem>
-        <ListItem sx = {{pt: 0, pl: 3}}>
-          <ListItemText primaryTypographyProps={{fontSize: '0.75em'}} >aaa@mail.com</ListItemText>
+        <ListItem sx={{ pt: 0, pl: 3 }}>
+          <ListItemText primaryTypographyProps={{ fontSize: '0.75em' }} >aaa@mail.com</ListItemText>
         </ListItem>
       </List>
 
-      <Divider/>
+      <Divider />
 
       <List onClick={toggleDrawer(anchor, false)}>
-      <ListItem>
-        <ListItemButton>
-          <ListItemText>Favorites</ListItemText>
-          <ListItemIcon  sx = {{ml: 1}}>
-            <FavoriteIcon />
-          </ListItemIcon>
-        </ListItemButton>
-      </ListItem>
+        <ListItem>
+          <ListItemButton>
+            <ListItemText>Favorites</ListItemText>
+            <ListItemIcon sx={{ ml: 1 }}>
+              <FavoriteIcon />
+            </ListItemIcon>
+          </ListItemButton>
+        </ListItem>
 
-      <ListItem>
-        <ListItemButton>
-        <ListItemText>Shopping cart</ListItemText>
-          <ListItemIcon  sx = {{ml: 1}}>
-            <ShoppingCartIcon />
-          </ListItemIcon>
-        </ListItemButton>
-      </ListItem>
+        <ListItem>
+          <ListItemButton>
+            <ListItemText>Shopping cart</ListItemText>
+            <ListItemIcon sx={{ ml: 1 }}>
+              <ShoppingCartIcon />
+            </ListItemIcon>
+          </ListItemButton>
+        </ListItem>
 
-      <ListItem>
-        <ListItemButton>
-        <ListItemText>Manage account</ListItemText>
-          <ListItemIcon  sx = {{ml: 1}}>
-            <PersonIcon />
-          </ListItemIcon>
-        </ListItemButton>
-      </ListItem>
+        <ListItem>
+          <ListItemButton>
+            <ListItemText>Manage account</ListItemText>
+            <ListItemIcon sx={{ ml: 1 }}>
+              <PersonIcon />
+            </ListItemIcon>
+          </ListItemButton>
+        </ListItem>
       </List>
 
-      <Divider/>
+      <Divider />
 
-      <List  onClick={toggleDrawer(anchor, false)}>
-      <ListItem>
-        <ListItemButton>
-          <ListItemText>About us</ListItemText>
-        </ListItemButton>
-      </ListItem>
+      <List onClick={toggleDrawer(anchor, false)}>
+        <ListItem>
+          <ListItemButton>
+            <ListItemText>About us</ListItemText>
+          </ListItemButton>
+        </ListItem>
 
-      <ListItem>
-        <ListItemButton>
-          <ListItemText>Contact us</ListItemText>
-        </ListItemButton>
-      </ListItem>
+        <ListItem>
+          <ListItemButton>
+            <ListItemText>Contact us</ListItemText>
+          </ListItemButton>
+        </ListItem>
       </List>
     </Box>
   );
 
   return (
     <div className="App">
+      <AppBar position="absolute" sx={{ width: "100%", height: "10%", p: 1, mb: 5 }}>
+        <Toolbar key={"left"} >
+          <IconButton
+            size="large"
+            edge="start"
+            color="inherit"
+            aria-label="menu"
+            sx={{ mr: 2 }}
+            onClick={toggleDrawer("left", true)}
+          >
+            <MenuIcon fontSize="large" sx={{ pr: 2, pl: 2 }} />
+          </IconButton>
 
+          <SwipeableDrawer
+            anchor={"left"}
+            open={state["left"]}
+            onClose={toggleDrawer("left", false)}
+            onOpen={toggleDrawer("left", true)}
+          >
+            {list("left")}
+          </SwipeableDrawer>
 
-      <Box>
-
-        <div>
-          {['left'].map(() => (
-            <React.Fragment >
-              <Button >{"left"}</Button>
-
-            </React.Fragment>
-          ))}
-        </div>
-
-        <AppBar sx={{ height: "10%" }}>
-          <Toolbar key={"left"} >
-
-            <IconButton
-              size="large"
-              edge="start"
-              color="inherit"
-              aria-label="menu"
-              sx={{ mr: 2 }}
-              onClick={toggleDrawer("left", true)}
-            >
-              <MenuIcon fontSize="large" sx={{ pr: 2, pl: 2 }} />
-            </IconButton>
-
-            <SwipeableDrawer
-              anchor={"left"}
-              open={state["left"]}
-              onClose={toggleDrawer("left", false)}
-              onOpen={toggleDrawer("left", true)}
-            >
-              {list("left")}
-            </SwipeableDrawer>
-
-            <Grid container spacing={2} alignItems="center" justifyContent="center">
-              <Grid item>
-                <Box
-                  component="img"
-                  sx={{
-                    ml: 1,
-                    height: "4%",
-                    width: "4%",
-                  }}
-                  src=""
-                  alt="logo"
-                />
-              </Grid>
-              <Grid item>
-                <Typography variant="h4" component="div" sx={{ flexGrow: 1 }}>
-                  Gnomes shop
-                </Typography>
-              </Grid>
+          <Grid container spacing={2} alignItems="center" justifyContent="center">
+            <Grid item>
+              <Box
+                component="img"
+                sx={{
+                  ml: 1,
+                  height: "4%",
+                  width: "4%",
+                }}
+                src=""
+                alt="logo"
+              />
             </Grid>
+            <Grid item>
+              <Typography variant="h4" component="div" sx={{ flexGrow: 1 }}>
+                Gnomes shop
+              </Typography>
+            </Grid>
+          </Grid>
 
 
-            <FavoriteIcon color="inherit" fontSize="large" sx={{ pr: 2, pl: 2 }} />
-            <ShoppingCartIcon color="inherit" fontSize="large" sx={{ pr: 2, pl: 2 }} />
-            <PersonIcon color="inherit" fontSize="large" sx={{ pr: 2, pl: 2 }} />
+          <FavoriteIcon color="inherit" fontSize="large" sx={{ pr: 2, pl: 2 }} />
+          <ShoppingCartIcon color="inherit" fontSize="large" sx={{ pr: 2, pl: 2 }} />
+          <PersonIcon color="inherit" fontSize="large" sx={{ pr: 2, pl: 2 }} />
 
-          </Toolbar>
-        </AppBar>
-      </Box>
+        </Toolbar>
+      </AppBar>
+
+      <Divider sx={{ pt: 12 }} textAlign="center" role="presentation">
+        <TextField label="What are you looking for?" color="primary" sx={{width:"1000px"}} />
+      </Divider>
+
+
 
 
     </div>
